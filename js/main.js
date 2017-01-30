90557682df2da624eff48526b30f406de93ad354
Gautier Maire <gautmaire45@gmail.com>
2017-01-30 15:24:30 +0100

    demo, sans canvas. préparation envirronnement de travail

--- js/main.js
+++ js/main.js
@@ -1,60 +1,108 @@
-var $results = $("#results");
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var $canvas = $("#canvas");
-var canvasOffset = $canvas.offset();
-var offsetX = canvasOffset.left;
-var offsetY = canvasOffset.top;
-
-var text = $("canvas p").text();
-var words = text.split(" ");
-var wordsBB = new Array(words.length);
-
-ctx.font = "30px arial";
-
-var length = ctx.measureText(words[0]).width;
-
-wordsBB[0] = {
-    x: 0,
-    y: 0,
-    width: length,
-    height: 200
-}
-
-var accumLength = length;
-
-for (var i = 1; i < words.length; i++) {
-    var length = ctx.measureText(" " + words[i]).width;
-    wordsBB[i] = {
-        x: accumLength,
-        y: 0,
-        width: length,
-        height: 200
-    }
-    accumLength += length;
-}
-
-ctx.fillText(text, 0, 30);
-ctx.lineWidth = 0.50;
+//  /* ----------------------------------
+//  Vérification position de la souris dans la console
+//  ---------------------------------- */
+//
+/*function mouse_position(event)
+{
+    console.log("CA MARCHE");
+}*/
 
 
-function handleMouseMove(e) {
-    e.preventDefault();
-    var x = parseInt(e.clientX - offsetX);
-    var y = parseInt(e.clientY - offsetY);
 
-    for (var i = 0; i < wordsBB.length; i++) {
-        var bb = wordsBB[i];
-        if (x > bb.x && x < bb.x + bb.width && y > bb.y & y < bb.y + bb.height) {
-            ctx.fillStyle="rgba(0, 0, 0, 0.1)";
-            ctx.fillRect(bb.x, bb.y, bb.width, bb.height);
-            $results.text(words[i]);
-            wordsBB.splice(i, 1);
-            words.splice(i, 1);         
-        }
-    }
-}
+//  /* ----------------------------------
+//          METHODE POUR AVOIR LA POSITION D'UN ELEMENT HTML
+//  ---------------------------------- */
+//
+// jQuery.fn.extend({
+//    findPos : function() {
+//        obj = jQuery(this).get(0);
+//        var curleft = obj.offsetLeft || 0;
+//        var curtop = obj.offsetTop || 0;
+//        while (obj = obj.offsetParent) {
+//                 curleft += obj.offsetLeft
+//                 curtop += obj.offsetTop
+//        }
+//        return {x:curleft,y:curtop};
+//    }
+// });
+//
+// $( function() {
+//  pos = $('#pTest').findPos();
+//  //retourne le coordonnées
+//  alert('x : '+pos.x+' / y : '+pos.y);
+//
+//  pos = $('#span').findPos();
+//  //retourne le coordonnées
+//  alert('x : '+pos.x+' / y : '+pos.y);
+//
+//
+// });
 
-$("#canvas").mousemove(function (e) {
-    handleMouseMove(e);
+
+
+//  /* ----------------------------------
+//      METHODE POUR TRANSFORMER METTRE TOUS LES MOTS DANS UNE SPAN
+//  ---------------------------------- */
+//
+// $( function(){
+//  // récupération du texte d'un <p>
+//   var chaine = $('#testChaine').text();
+//
+//  // initialisation de chaque caractère dans une case d'un tableau
+//  var tabChaine = new Array();
+//  for(var i=0; i<=chaine.length; i++){
+//      tabChaine[i] = chaine[i];
+//  }
+//  alert(tabChaine);
+//
+//  // Ajout d'une <span> pour le début du texte
+//  tabChaine.splice(0,0,"<span>");
+//  //
+//  for(var i=0; i<=tabChaine.length; i++){
+//      // simple côte fonctionne bizarrement
+//      if(tabChaine[i] == " " || tabChaine[i] == "," || tabChaine[i] == "(" || tabChaine[i] == ")" || tabChaine[i] == "\""){
+//          tabChaine.splice(i,1,"</span> <span>");
+//
+//      }
+//
+//  }
+//  // Ajout d'une </span> pour la fin du texte
+//  tabChaine.splice(tabChaine.lenght+1,0,"</span>");
+//  console.log(tabChaine)
+//  alert(tabChaine);
+//
+//
+//  // transformation en chaine de charactère après traitement
+//  var chaineAfter = "";
+//  for(var i=0; i<=tabChaine.length; i++){
+//      chaineAfter = chaineAfter+tabChaine[i];
+//  }
+//
+//  alert(chaineAfter);
+// });
+//
+//
+//  /* ----------------------------------
+// METHODE QUI FONCTION POUR L'AJOUT DES SPAN
+//  ---------------------------------- */
+//
+$( function(){
+    var words = $("#testChaine").text().split(" ");
+    $("#testChaine").empty();
+    $.each(words, function(i, v) {
+        $("#testChaine").append($("<span>").text(v));
+        $("#testChaine").append(" ");
+    });
+});
+
+
+//  /* ----------------------------------
+//      METHODE POUR MODIFIER LA COULEUR DE TOUT LES SPAN AU SURVOLE
+//  ---------------------------------- */
+//
+$( function(){
+    $("span").mouseover( function(){
+        $(this).css("background", "#40A497");
+    });
+
 });

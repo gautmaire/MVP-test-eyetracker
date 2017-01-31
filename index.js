158968068287c46fd80076a6ed5e8301cfbb1b6a
Gautier Maire <gautmaire45@gmail.com>
2017-01-31 08:36:45 +0100

    Demo plus ou moins intégrer (css et js dans index.html)

---
+++ index.js
@@ -0,0 +1,171 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var left, right; // left.x left.y etc, positions of the gaze
+
+// pour essayer d'inclure les fichiers
+/*var routes = require("./static/css/bootstrap.min.css"); 
+app.use('/', routes);
+
+var routes1 = require("./static/css/style.css"); 
+app.use('/', routes1);
+
+var routes2 = require("./static/js/main.js"); 
+app.use('/', routes2);
+
+var routes3 = require("./static/js/main.js"); 
+app.use('/', routes3);*/
+
+
+
+//--- GazeJs ----
+//---------------
+
+var callbackTypes = require("./lib_gazejs//tobii/callback_types");
+var bridjs = require("bridjs");
+var errorCode = new bridjs.NativeValue.uint32();
+var ErrorCodes = require("./lib_gazejs/tobii/error_codes");
+var TobiiGaze = require("./lib_gazejs/tobii/gaze");
+var gaze = new TobiiGaze();
+var dataTypes = require("./lib_gazejs/tobii/data_types"), log4js = require("log4js");
+var log = log4js.getLogger("GazeJSTest");
+var modelUrl = new Buffer(dataTypes.Constants.DEVICE_INFO_MAX_MODEL_LENGTH);
+var eyeTracker, eventLoopErrorCode = new bridjs.NativeValue.uint32();
+
+var onConnectCallback,onStopTrackingCallback,onDeviceInfoCallback,onStartTrackingCallback,
+    onGazeDataCallback;
+
+var checkError = function (errorCode) {
+    var value;
+
+    if (typeof (errorCode) === "number") {
+        value = errorCode;
+    } else {
+        value = errorCode.get();
+    }
+
+    if (value !== ErrorCodes.SUCCESS) {
+        throw new GazeException("Gaze error :" + gaze.getErrorMessage(value));
+    }
+}
+
+
+function startTracking(){
+    var deviceInfo = new dataTypes.DeviceInfo();
+    
+    gaze.connect(eyeTracker, bridjs.byPointer(errorCode));
+    checkError(errorCode);
+    
+    gaze.getDeviceInfo(eyeTracker, bridjs.byPointer(deviceInfo),bridjs.byPointer(errorCode));
+    checkError(errorCode);
+    
+    log.info("OnDeviceInof, serial number = "+bridjs.toString(deviceInfo.serialNumber));
+    
+    gaze.startTracking(eyeTracker,onGazeDataCallback,bridjs.byPointer(errorCode), null);
+    checkError(errorCode);
+    
+    // setTimeout(function(){
+    //     stopTracking();
+    // }, 20000);
+    //tobiigaze_start_tracking_async(eye_tracker, &start_tracking_callback, &on_gaze_data, 0);
+}
+
+function stopTracking(){
+    gaze.stopTracking(eyeTracker,bridjs.byPointer(errorCode));
+    checkError(errorCode);
+    
+    gaze.disconnect(eyeTracker);
+    
+    gaze.breakEventLoop((eyeTracker));
+}
+
+function destroy() {
+    log.info("Destroy eyeTracker: "+eyeTracker);
+    
+    if (eyeTracker) {
+        gaze.destroy((eyeTracker));
+    }
+
+    //config.free();
+
+    log.info("done");
+    
+    process.exit();
+}
+
+try {
+    //config.init(bridjs.byPointer(errorCode));
+   // gazejs.checkError(errorCode);
+    gaze.getConnectedEyeTracker(modelUrl, dataTypes.Constants.DEVICE_INFO_MAX_MODEL_LENGTH,
+            bridjs.byPointer(errorCode));
+    checkError(errorCode);        
+    //config.getDefaultEyeTrackerUrl(modelUrl, dataTypes.Constants.DEVICE_INFO_MAX_MODEL_LENGTH,
+    //        bridjs.byPointer(errorCode));
+    //gazejs.checkError(errorCode);
+    //modelUrl.write("--auto\0");
+    log.info("Model name: " + bridjs.toString(modelUrl));
+
+    log.info("Natvie library version: " + gaze.getVersion());
+
+    eyeTracker = gaze.create(modelUrl, bridjs.byPointer(errorCode));
+    checkError(errorCode);
+    
+    gaze.setLogging("log.txt", dataTypes.LogLevel.OFF, bridjs.byPointer(errorCode));
+    checkError(errorCode);
+    log.info("Start event loop");
+    bridjs.async(gaze).runEventLoop((eyeTracker),
+        bridjs.byPointer(eventLoopErrorCode), function(){
+        checkError(eventLoopErrorCode);
+        log.info("gaze.runEventLoop() returned");
+        
+        destroy();
+    });
+    
+    /*Delay execution to workaround strange crash issue*/
+    setTimeout(function(){
+        gaze.registerError((eyeTracker), onErrorCallback, null);
+        startTracking();
+    },16);
+} catch (e) {
+    log.error(e);
+
+    destroy();
+}
+
+
+
+
+
+onErrorCallback = bridjs.newCallback(callbackTypes.AsyncCallback, function(errorCode, userData) {
+    log.info("OnError: "+gaze.getErrorMessage(errorCode));
+    
+    setTimeout(destroy, 0);
+});
+
+
+onGazeDataCallback = bridjs.newCallback(callbackTypes.Listener, function(gazeData, extensions, userData) {
+    left = gazeData.left.gazePointOnDisplayNormalized, right = gazeData.right.gazePointOnDisplayNormalized;
+});
+
+//-----------------------
+
+
+
+
+
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+//When request from the client
+io.on('connection', function(socket){
+    socket.on('updateRequest', function(msg){
+	io.emit('coordinates', left.x, left.y, right.x, right.y);
+    });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ static/js/main.js
@@ -0,0 +1,109 @@
+//  /* ----------------------------------
+//  Vérification position de la souris dans la console
+//  ---------------------------------- */
+//
+/*function mouse_position(event)
+{
+    console.log("CA MARCHE");
+}*/
+
+
+
+//  /* ----------------------------------
+//          METHODE POUR AVOIR LA POSITION D'UN ELEMENT HTML
+//  ---------------------------------- */
+//
+// jQuery.fn.extend({
+//    findPos : function() {
+//        obj = jQuery(this).get(0);
+//        var curleft = obj.offsetLeft || 0;
+//        var curtop = obj.offsetTop || 0;
+//        while (obj = obj.offsetParent) {
+//                 curleft += obj.offsetLeft
+//                 curtop += obj.offsetTop
+//        }
+//        return {x:curleft,y:curtop};
+//    }
+// });
+//
+// $( function() {
+//  pos = $('#pTest').findPos();
+//  //retourne le coordonnées
+//  alert('x : '+pos.x+' / y : '+pos.y);
+//
+//  pos = $('#span').findPos();
+//  //retourne le coordonnées
+//  alert('x : '+pos.x+' / y : '+pos.y);
+//
+//
+// });
+
+
+
+//  /* ----------------------------------
+//      METHODE POUR TRANSFORMER METTRE TOUS LES MOTS DANS UNE SPAN
+//  ---------------------------------- */
+//
+// $( function(){
+//  // récupération du texte d'un <p>
+//   var chaine = $('#testChaine').text();
+//
+//  // initialisation de chaque caractère dans une case d'un tableau
+//  var tabChaine = new Array();
+//  for(var i=0; i<=chaine.length; i++){
+//      tabChaine[i] = chaine[i];
+//  }
+//  alert(tabChaine);
+//
+//  // Ajout d'une <span> pour le début du texte
+//  tabChaine.splice(0,0,"<span>");
+//  //
+//  for(var i=0; i<=tabChaine.length; i++){
+//      // simple côte fonctionne bizarrement
+//      if(tabChaine[i] == " " || tabChaine[i] == "," || tabChaine[i] == "(" || tabChaine[i] == ")" || tabChaine[i] == "\""){
+//          tabChaine.splice(i,1,"</span> <span>");
+//
+//      }
+//
+//  }
+//  // Ajout d'une </span> pour la fin du texte
+//  tabChaine.splice(tabChaine.lenght+1,0,"</span>");
+//  console.log(tabChaine)
+//  alert(tabChaine);
+//
+//
+//  // transformation en chaine de charactère après traitement
+//  var chaineAfter = "";
+//  for(var i=0; i<=tabChaine.length; i++){
+//      chaineAfter = chaineAfter+tabChaine[i];
+//  }
+//
+//  alert(chaineAfter);
+// });
+//
+//
+//  /* ----------------------------------
+// METHODE QUI FONCTION POUR L'AJOUT DES SPAN
+//  ---------------------------------- */
+//
+$( function(){
+    var words = $("#pTest").text().split(" ");
+    $("#pTest").empty();
+    $.each(words, function(i, v) {
+        $("#pTest").append($("<span>").text(v));
+        $("#pTest").append(" ");
+    });
+});
+
+
+//  /* ----------------------------------
+//      METHODE POUR MODIFIER LA COULEUR DE TOUT LES SPAN AU SURVOLE
+//  ---------------------------------- */
+//
+$( function(){
+    $("span").mouseover( function(){
+        $(this).css("color", "#F6CD61");
+    });
+
+});
+

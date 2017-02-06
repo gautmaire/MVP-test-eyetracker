8387ef2ca5abf315cd822e64e8d799877e7ed80c
Gautier Maire <gautmaire45@gmail.com>
2017-02-06 11:28:06 +0100

    combinaison des intégrations

--- js/main.js
+++ js/main.js
@@ -1,60 +1,33 @@
-var $results = $("#results");
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var $canvas = $("#canvas");
-var canvasOffset = $canvas.offset();
-var offsetX = canvasOffset.left;
-var offsetY = canvasOffset.top;
+$(document).ready(function() {
 
-var text = $("canvas p").text();
-var words = text.split(" ");
-var wordsBB = new Array(words.length);
-
-ctx.font = "30px arial";
-
-var length = ctx.measureText(words[0]).width;
-
-wordsBB[0] = {
-    x: 0,
-    y: 0,
-    width: length,
-    height: 200
-}
-
-var accumLength = length;
-
-for (var i = 1; i < words.length; i++) {
-    var length = ctx.measureText(" " + words[i]).width;
-    wordsBB[i] = {
-        x: accumLength,
-        y: 0,
-        width: length,
-        height: 200
-    }
-    accumLength += length;
-}
-
-ctx.fillText(text, 0, 30);
-ctx.lineWidth = 0.50;
+	clickedFunction("concept");
+	clickedFunction("comCaMarche");
+	clickedFunction("eyetracker");
+	clickedFunction("temoignages");
+	clickedFunction("twitter");
+	clickedFunction("equipe");
+	clickedFunction("mentions-legales");
 
 
-function handleMouseMove(e) {
-    e.preventDefault();
-    var x = parseInt(e.clientX - offsetX);
-    var y = parseInt(e.clientY - offsetY);
-
-    for (var i = 0; i < wordsBB.length; i++) {
-        var bb = wordsBB[i];
-        if (x > bb.x && x < bb.x + bb.width && y > bb.y & y < bb.y + bb.height) {
-            ctx.fillStyle="rgba(0, 0, 0, 0.1)";
-            ctx.fillRect(bb.x, bb.y, bb.width, bb.height);
-            $results.text(words[i]);
-            wordsBB.splice(i, 1);
-            words.splice(i, 1);         
-        }
-    }
-}
-
-$("#canvas").mousemove(function (e) {
-    handleMouseMove(e);
 });
+
+
+function clickedFunction(element){
+	$("#"+element).on( "click", function()
+	{
+		$( "aside a.activeLink").removeClass( "activeLink" );
+		$( "aside a#"+element).addClass( "activeLink" );
+
+		// Cache les sections non selectionner a gauche
+		$( "div > section" ).removeClass( "activeSection" ).addClass("desactiveSection");
+		
+		// ajouter le déplacement du menu a gauche
+		$( "div > aside").addClass("activeItem");
+		// Cacher les section non selectionner
+		$( ".cache" ).addClass("activeCache");
+
+		// rendre visible la section selectionner
+		$( "."+element).addClass("activeSection").removeClass( "desactiveSection" );;
+
+	});
+}

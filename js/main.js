a1ad4152983f8bf4e6af832ce33aa30904d69a91
Gautier Maire <gautmaire45@gmail.com>
2017-02-07 20:04:57 +0100

    correction html css des 3 page principales

--- js/main.js
+++ js/main.js
@@ -1,18 +1,44 @@
+//***************************/
+// Menu de la page d'accueil
+//***************************/
+
 $(document).ready(function() {
 
-	clickedFunction("concept");
-	clickedFunction("comCaMarche");
-	clickedFunction("eyetracker");
-	clickedFunction("temoignages");
-	clickedFunction("twitter");
-	clickedFunction("equipe");
-	clickedFunction("mentions-legales");
+	menuAccueil("concept");
+	menuAccueil("comCaMarche");
+	menuAccueil("eyetracker");
+	menuAccueil("temoignages");
+	menuAccueil("twitter");
+	menuAccueil("equipe");
+	menuAccueil("mentions-legales");
 
+	/* menu user */
+	menuUsers("us-tab-bord");
+	menuUsers("us-textes");
+	menuUsers("us-question");
+	menuUsers("us-twitter");
+	menuUsers("us-mentions-legales");
 
+	$(".buttonBurger").on( "click", function()
+	{
+		if( $( ".menu" ).hasClass( "activeMenuUsers" ) && $( ".buttonBurger" ).hasClass( "activeButBurger" ) )
+		{
+			// Cacher le menu Users a gauche
+			$( ".menu" ).removeClass( "activeMenuUsers" );
+			$(".buttonBurger").removeClass( "activeButBurger" );
+		}else{
+			// rendre visible le menu user
+			$( ".menu").addClass("activeMenuUsers");
+			$(".buttonBurger").addClass( "activeButBurger" );
+		}
+		
+		
+	})
+	
 });
 
 
-function clickedFunction(element){
+function menuAccueil(element){
 	$("#"+element).on( "click", function()
 	{
 		$( "aside a.activeLink").removeClass( "activeLink" );
@@ -27,7 +53,29 @@ function clickedFunction(element){
 		$( ".cache" ).addClass("activeCache");
 
 		// rendre visible la section selectionner
-		$( "."+element).addClass("activeSection").removeClass( "desactiveSection" );;
+		$( "."+element).addClass("activeSection").removeClass( "desactiveSection" );
 
 	});
 }
+
+
+//***************************/
+// 		Menu users
+//***************************/
+
+$(".buttonBurger").click(function(){
+	$(".buttonBurger__top").toggleClass("buttonBurger__top--active");
+    $(".buttonBurger__middle").toggleClass("buttonBurger__middle--active");
+    $(".buttonBurger__bottom").toggleClass("buttonBurger__bottom--active");
+});
+
+function menuUsers(element){
+	$("#"+element).on( "click", function()
+	{
+		$( ".menu > nav a.activeLink").removeClass( "activeLink" );
+		$( ".menu > nav a#"+element).addClass( "activeLink" );
+		
+
+	});
+}
+
